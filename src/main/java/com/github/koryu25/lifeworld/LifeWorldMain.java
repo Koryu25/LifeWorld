d12da4fb541ede6aeeb0c4e4739c0843a3dd8452
Koryu25 <komu.ryu@icloud.com>
2022-03-19 18:51:55 +0900

    Initialize commit

---
+++ src/main/java/com/github/koryu25/lifeworld/LifeWorldMain.java
@@ -0,0 +1,17 @@
+package com.github.koryu25.lifeworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LifeWorldMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
