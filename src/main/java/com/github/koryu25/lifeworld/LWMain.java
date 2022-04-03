9ea1aca6fa036763983760e1b3655204738e123b
erudosan0524-JP <Eruvis4869>
2022-04-03 16:41:35 +0900

    プロジェクトのリセット

--- src/main/java/com/github/koryu25/lifeworld/LWMain.java
+++ src/main/java/com/github/koryu25/lifeworld/LWMain.java
@@ -1,16 +1,6 @@
 package com.github.koryu25.lifeworld;
 
-import com.github.koryu25.lifeworld.command.CommandManager;
-import com.github.koryu25.lifeworld.data.LWBlockDataSet;
-import com.github.koryu25.lifeworld.data.SqlDAO;
-import com.github.koryu25.lifeworld.listener.ListenerManager;
-import com.github.koryu25.lifeworld.player.LWPlayer;
-import com.github.koryu25.lifeworld.yaml.MainConfig;
-import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public final class LWMain extends JavaPlugin {
 
