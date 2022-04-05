b14b3d8df027841ee2f89ff26acf0a9d7717ca1b
Koryu25 <komu.ryu@icloud.com>
2022-04-05 11:43:25 +0900

    一部コードの修正、インポートの最適化、

--- src/main/java/com/github/koryu25/lifeworld/LWMain.java
+++ src/main/java/com/github/koryu25/lifeworld/LWMain.java
@@ -1,15 +1,8 @@
 package com.github.koryu25.lifeworld;
 
 import com.github.koryu25.lifeworld.command.CommandManager;
-import com.github.koryu25.lifeworld.item.LWItem;
-import com.github.koryu25.lifeworld.item.LWItemManager;
 import com.github.koryu25.lifeworld.listener.ListenerManager;
-import lombok.Getter;
-import org.bukkit.Location;
-import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.HashMap;
 
 public final class LWMain extends JavaPlugin {
 
